c106595787160884eb9ca743943e496820cbe84a
たいち ひ <taichi221228@icloud.com>
2024-12-17 19:41:37 +0900

    chore(bundler): Conditionally include Remix plugin based on NODE_ENV
    Wrapped the Remix plugin initialization to exclude it in test environments.

--- vite.config.ts
+++ vite.config.ts
@@ -12,15 +12,16 @@ declare module "@remix-run/node" {
 
 export default defineConfig({
   plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
+    process.env.NODE_ENV !== "test" &&
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+          v3_singleFetch: true,
+          v3_lazyRouteDiscovery: true,
+        },
+      }),
     tsconfigPaths(),
   ],
   test: {},
