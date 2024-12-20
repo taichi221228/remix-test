43dace8658cef238c81be906db5168d4c7ecb5a7
たいち ひ <taichi221228@icloud.com>
2024-12-20 19:47:49 +0900

    feat(test): Add test for the Index route component
    This commit introduces a unit test for the Index route component using Vitest and React Testing Library.

---
+++ app/routes/_index.test.tsx
@@ -0,0 +1,19 @@
+/** @vitest-environment jsdom */
+
+import "@testing-library/jest-dom/vitest";
+
+import { createRemixStub } from "@remix-run/testing";
+import { render, screen } from "@testing-library/react";
+
+import { default as Component } from "./_index";
+
+const path = "/";
+
+test("render <Index />", () => {
+  const App = createRemixStub([{ path, Component }]);
+
+  render(<App initialEntries={[path]} />);
+
+  const target = screen.getByRole("heading", { level: 1 });
+  expect(target).toHaveTextContent("Welcome to Remix");
+});
