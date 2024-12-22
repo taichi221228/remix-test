e752fbec8166d121965b022de037f1423e8db6e4
たいち ひ <taichi221228@icloud.com>
2024-12-22 20:11:13 +0900

    refactor: Remove debug statement from Index component test
    The unnecessary `screen.debug()` call was removed to clean up the test file.

--- app/routes/static/_index.react.test.tsx
+++ app/routes/static/_index.react.test.tsx
@@ -9,8 +9,6 @@ import { default as Component } from "./_index";
 test("render <Index /> without Remix", () => {
   render(<Component />);
 
-  screen.debug();
-
   const target = screen.getByRole("heading", { level: 1 });
   expect(target).toHaveTextContent("Welcome to Remix");
 });
